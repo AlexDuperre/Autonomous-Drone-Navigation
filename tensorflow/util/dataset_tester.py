bef48972ed830c922facae38df31a4e9a50e2703
Alexandre <alex.duperre@hotmail.com>
2020-01-31 14:09:04 -0500

    restructuring

--- tensorflow/util/dataset_tester.py
+++ tensorflow/util/dataset_tester.py
@@ -1,9 +1,10 @@
 import h5py
 import cv2
 import numpy as np
+import matplotlib.pylab as plt
 from matplotlib.pylab import cm
 
-f = h5py.File("../Batch_1.h5", "r")
+f = h5py.File("../dataset/ardrone_luxury_home/path_0_1_000.h5", "r")
 
 keys = list(f.keys())
 
@@ -13,10 +14,14 @@ for key in keys:
     print(f[key])
 
 print(f["video"][0])
-image = f["video"][0]
-image = ((image - image.min()) * (1 / (6 - 0) * 255)).astype('uint8')
-image = np.uint8(cm.jet(image)*255)
-image = cv2.cvtColor(image,cv2.COLOR_RGBA2BGR)
-cv2.imshow("Depth", image)
+image = f["video"][0][0]
+# image = ((image - image.min()) * (1 / (6 - 0) * 255)).astype('uint8')
+# image = np.uint8(cm.jet(image)*255)
+# image = cv2.cvtColor(image,cv2.COLOR_RGBA2BGR)
+cv2.imshow("Depth", image/255)
 
 cv2.waitKey(0)
+
+# print(image.shape)
+# plt.imshow(image)
+# plt.show()
