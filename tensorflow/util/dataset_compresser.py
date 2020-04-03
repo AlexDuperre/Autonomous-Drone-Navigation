cbaa34c89167776058c736a54ad7df3718315d0d
Alexandre <alex.duperre@hotmail.com>
2020-04-03 11:52:04 -0400

    updated dataset copressor

--- tensorflow/util/dataset_compresser.py
+++ tensorflow/util/dataset_compresser.py
@@ -16,7 +16,7 @@ from functools import reduce
 
 def main(file_path):
 
-    new_path = "/media/aldupd/UNTITLED 2/light dataset"
+    new_path = "/media/aldupd/UNTITLED 2/light dataset V2"
 
     world_no = 9
     world_strings = ["luxury_home", "luxury_home_2e_floor", "bar", "machine_room", "mechanical_plant", "office",
@@ -31,7 +31,7 @@ def main(file_path):
     # file_path = "./path_7_5_001.h5"
     print("Working on : ", file_path)
 
-    with h5py.File(file_path, "a") as f:
+    with h5py.File(file_path, "r") as f:
         depth = f["depth"][:]
         rel_orientation = f["rel_orientation"][:]
         rel_goalx = f["rel_goalx"][:]
@@ -42,12 +42,12 @@ def main(file_path):
     title = new_path + "/" + file_path.split("/")[-2] + "/" + file_path.split("/")[-1]
     print("copied to ; ",title)
     with h5py.File(title, "w") as hdf:
-        hdf.create_dataset("depth", data=depth, compression="gzip")
-        hdf.create_dataset("rel_goalx", data=rel_goalx, compression="gzip")
-        hdf.create_dataset("rel_goaly", data=rel_goaly, compression="gzip")
-        hdf.create_dataset("goal_orientation", data=goal_orientation, compression="gzip")
-        hdf.create_dataset("rel_orientation", data=rel_orientation, compression="gzip")
-        hdf.create_dataset("GT", data=GT, compression="gzip")
+        hdf.create_dataset("depth", data=depth, chunks=None)
+        hdf.create_dataset("rel_goalx", data=rel_goalx, chunks=None)
+        hdf.create_dataset("rel_goaly", data=rel_goaly, chunks=None)
+        hdf.create_dataset("goal_orientation", data=goal_orientation, chunks=None)
+        hdf.create_dataset("rel_orientation", data=rel_orientation, chunks=None)
+        hdf.create_dataset("GT", data=GT, chunks=None)
 
 
 if __name__ == '__main__':
